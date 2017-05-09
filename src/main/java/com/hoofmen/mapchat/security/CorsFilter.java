f8f7fee761c5d945f36400879d329cd31e3c2578
hoofmen <hoofmen@gmail.com>
2017-05-09 16:28:59 -0700

    Cors filter

--- src/main/java/com/hoofmen/mapchat/security/CorsFilter.java
+++ src/main/java/com/hoofmen/mapchat/security/CorsFilter.java
@@ -1,9 +1,6 @@
 package com.hoofmen.mapchat.security;
 
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -11,23 +8,27 @@ import java.io.IOException;
 /**
  * Created by Osman H. on 1/17/17.
  */
-public class CorsFilter extends OncePerRequestFilter {
+public class CorsFilter implements Filter {
 
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
 
-    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                 FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
 
-        setResponseHeaders(request, response);
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        resp.addHeader("Access-Control-Allow-Origin","*");
+        resp.addHeader("Access-Control-Allow-Methods","GET,POST");
+        resp.addHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
 
-        if (null != chain) {
-            chain.doFilter(request, response);
+        // Just ACCEPT and REPLY OK if OPTIONS
+        if ( request.getMethod().equals("OPTIONS") ) {
+            resp.setStatus(HttpServletResponse.SC_OK);
+            return;
         }
+        chain.doFilter(request, servletResponse);
     }
 
-    private void setResponseHeaders(HttpServletRequest request, HttpServletResponse response) {
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin") );
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-Requested-With");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-    }
+    @Override
+    public void destroy() {}
 }
